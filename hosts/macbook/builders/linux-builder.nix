9a8c06e7ee7dc6d5ca5490d2f161c1ece9598a95
Robert Cambridge <robert@cambridge.me>
2025-11-26 22:21:35 +0100

    experiment with linux-builder-x86_64

--- hosts/macbook/builders/linux-builder.nix
+++ hosts/macbook/builders/linux-builder.nix
@@ -1,4 +1,4 @@
-let
+{ pkgs, ... }: let
   builderEmulatesSystems = [
     "x86_64-linux"
     # "armv7l-linux"
@@ -12,20 +12,21 @@ in
     systems = [ "aarch64-linux" ] ++ builderEmulatesSystems;
     maxJobs = 100;
     speedFactor = 1;
+    package = pkgs.darwin.linux-builder-x86_64;
 
     # comment this block if the vm fails to build: the vm in the binary cache
     # doesn't have any customisations so nix tries to build one from source, but
     # that can't be done without already having an existing vm to build it.
-    config = ({ pkgs, ... }:{
-      boot.binfmt.emulatedSystems = builderEmulatesSystems;
-      nix.gc.automatic = true;
-      virtualisation = {
-        darwin-builder = {
-          diskSize = 100 * 1024;
-          memorySize = 4 * 1024;
-        };
-        cores = 6;
-      };
-    });
+    # config = ({ pkgs, ... }:{
+    #   boot.binfmt.emulatedSystems = builderEmulatesSystems;
+    #   nix.gc.automatic = true;
+    #   virtualisation = {
+    #     darwin-builder = {
+    #       diskSize = 100 * 1024;
+    #       memorySize = 4 * 1024;
+    #     };
+    #     cores = 6;
+    #   };
+    # });
   };
 }
