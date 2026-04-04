1f2af9bcb1c86fcbf413a525437694b3fd1a24f0
Robert Cambridge <robert@cambridge.me>
2026-04-04 23:27:37 +0200

    fix websearch tool. backup opencode and model config

--- hosts/macbook/inference.nix
+++ hosts/macbook/inference.nix
@@ -1,10 +1,43 @@
-{ perSystem, ... }: {
+{ lib, pkgs, perSystem, ... }: {
   services.ollama = {
     enable = true;
     package = perSystem.nixpkgs-ollama.ollama;
   };
 
-  environment.systemPackages = [
+  environment.systemPackages = let
+    opencodeWithSearch = writeScriptBin "opencode" ''
+      OPENCODE_ENABLE_EXA=1 ${lib.getExe opencode}
+    '';
+  in with pkgs; [
     perSystem.claude-code-nix.default
+    opencodeWithSearch
   ];
+
+  # ~/.config/opencode
+  # "permission": {
+  #   "websearch": "ask",
+  #   "webfetch": "ask",
+  #   "bash": {
+  #     "*": "ask",
+  #     "ls *": "allow",
+  #     "cat *": "allow",
+  #     "echo *": "allow",
+  #     "head *": "allow",
+  #     "tail *": "allow",
+  #     "grep *": "allow",
+  #     "sort *": "allow",
+  #     "jq *": "allow",
+  #     "wc *": "allow",
+  #     "git diff *": "allow",
+  #     "git log *": "allow",
+  #     "kubectl get *": "allow",
+  #     "kubectl logs *": "allow",
+  #     "tar *": "allow"
+  #   }
+  # }
+
+  # ~/.config/AGENTS.md
+  # # Tool Preferences
+  # * When fetching URLs or web content, always use the `webfetch`, NEVER use bash commands.
+  # * You're encouraged to use the `websearch` tool
 }
