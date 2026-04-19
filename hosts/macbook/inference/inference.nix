432a25981c26a3d5c9e80424db53b5c51e438824
Robert Cambridge <robert@cambridge.me>
2026-04-19 22:39:58 +0200

    opencode-sandbox is sort of stable!

--- hosts/macbook/inference/inference.nix
+++ hosts/macbook/inference/inference.nix
@@ -1,23 +1,12 @@
-{ config, flake, inputs, lib, pkgs, perSystem, ... }: {
+{ config, flake, inputs, lib, pkgs, perSystem, ... }:
+with flake.lib;
+{
   imports = [
     flake.darwinModules.lmstudio
     inputs.opencode-sandbox.nixosModules.opencode-sandbox
   ];
 
   age.secrets.opencode-go-api-key.file = ../../../secrets/opencode-go-api-key.age;
-  age-template.files.opencode-sandbox-env = {
-    vars = {
-      opencode_go_api_key = config.age.secrets.opencode-go-api-key.path;
-    };
-    content = ''
-      OPENCODE_ENABLE_EXA=1
-      OPENCODE_API_KEY=$opencode_go_api_key
-      OPENAI_API_KEY=
-      ZHIPU_API_KEY=
-    '';
-    # owner = "opencode";
-    # group = "opencode";
-  };
 
   services.ollama = {
     enable = true;
@@ -52,7 +41,9 @@
   programs.opencode-sandbox = {
     enable = true;
 
-    envFile = config.age-template.files.opencode-sandbox-env.path;
+    envFile = "/Users/${macbook.main-user}/.config/opencode-sandbox/env";
+    dataDir = "/Users/${macbook.main-user}/.config/opencode-sandbox/data";
+    cacheDir = "/Users/${macbook.main-user}/.config/opencode-sandbox/cache";
 
     configDir = let
       opencode-json = pkgs.writeText "opencode.json" (builtins.toJSON {
@@ -63,40 +54,29 @@
           "*" = "allow";
         };
         default_agent = "plan";
-
+        agent = {
+          plan = {
+            mode =  "primary";
+            model =  "openai/gpt-5.4";
+          };
+          build =  {
+            mode =  "primary";
+            model =  "opencode-go/qwen3.5-plus";
+          };
+        };
         instructions = [
           "https://raw.githubusercontent.com/rcambrj/opencode/refs/heads/main/AGENTS.md"
         ];
-
-        # provider/model examples
-        provider = {
-          opencode-go.models."qwen3.5-plus" = {};
-          openai.models."gpt-5.4" = {};
-          zai-coding-plan.model."glm-5.1" = {};
-          ollama = {
-            # Ollama is expected to run outside the sandbox VM.
-            # Set `baseURL` to an endpoint reachable from inside the guest.
-            # When Ollama is exposed on the VM's host, the QEMU default gateway is `10.0.2.2`:
-            options.baseURL = "http://10.0.2.2:11434/v1";
-            models."llama3.1" = {};
-          };
-        };
-
       });
+
+      extraModules = [
+        (pkgs: {
+          environment.systemPackages = [ pkgs.nix ];
+        })
+      ];
     in pkgs.runCommand "opencode-sandbox-config" {} ''
       mkdir -p "$out"
       cp ${opencode-json} "$out/opencode.json"
     '';
-
-    # optional
-    dataDir = /persist/opencode/data;
-    cacheDir = /persist/opencode/cache;
-    showBootLogs = false;
-    extraModules = [
-      {
-        # for `opencode-sandbox -- serve --hostname 0.0.0.0 --port 4096`
-        networking.firewall.allowedTCPPorts = [ 4096 ];
-      }
-    ];
   };
 }
